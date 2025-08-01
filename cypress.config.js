5a460e61d6d580d7f9fc34abfe445750c4a191e5
ivanskilledup <ivan@skilledup.life>
2025-08-01 04:24:45 +0100

    fixed ci

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,6 @@ module.exports = defineConfig({
       // implement node event listeners here
        // Ensure this matches your server's URL
     },
-    baseUrl: 'https://bookstoreapi-q86w.onrender.com',
+    baseUrl: 'http://localhost:5000',
   },
 });
